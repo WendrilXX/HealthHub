9aae31a18eada40c5941c4369249562b2234bbdb
Alcivan Lucas <61625442+AlcivanLucas@users.noreply.github.com>
2024-06-11 15:01:33 -0300

    Add files via upload

---
+++ src/pages/Forum.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Conversa from "../components/Conversa";
+function Forum() {
+  return (
+    <div>
+      <Conversa></Conversa>
+    </div>
+  );
+}
+
+export default Forum;
