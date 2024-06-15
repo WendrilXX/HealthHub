2e7358df1f93352dc124e2f457acb54d3ce69aa2
Wendril-not <wendriltutu@gmail.com>
2024-06-15 17:14:54 -0300

    feat: Adicionar estilos de cabeçalho e rodapé nas páginas Home e Sobre

---
+++ src/components/botao/Botao.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './botao.css';
+
+function Botao() {
+  const [color, setColor] = useState('white');
+
+  const handleClick = () => {
+    setColor(prevColor => (prevColor === 'white' ? 'black' : 'white'));
+  };
+
+  return (
+    <div>
+      <button 
+        className={`button ${color}`} 
+        onClick={handleClick}
+      >
+        Tab 1
+      </button>
+    </div>
+  );
+}
+
+export default Botao;
+
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,20 @@
-import Widget from '../../components/Widget'
-
-export function Home() {
-    // importar os elementos da pahina inicial
-    // Moço que musica boa do caralho, me desculpem 
-    // return <Widget />;
-    return (
-        <div>
-            {/* <Widget /> */}
-            <span className="text-lg font-bold " >App</span>
+import Header from "../../components/header/header";
+import Card from "../../components/card/card";
+import SobreNos from "../../components/sobre/sobre";
+import Footer from "../../components/footer/footer";
+import './home.css'
+function Home(){
+    return(
+        <div className="div-body">
+            <Header />
+            <div className="imagem">
+                <img src="../src/assets/autismo.png" alt="imagem-body" className="img-autismo"/>  
+            </div>
+            
+            <Card className="card-rosa"/>
+            <SobreNos />
+            <Footer />
         </div>
-      )
-
+    )
 }
-
-export default Home ;
+export default Home;
