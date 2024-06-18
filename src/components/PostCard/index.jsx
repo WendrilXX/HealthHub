7931072bb338b42c839a5101ca710bce41b868bb
heitorviana-dev <heitorsoaresviana@gmail.com>
2024-06-18 09:52:48 -0300

    Adicionando funcionalidade de clique em Recursos

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -3,9 +3,16 @@ import './styles.css';
 export const PostCard = (props) => {
     const { post } = props;
 
+    const handleClick = (e) => {
+        const url = post.url.trim();
+        if (url) {
+            window.open(url, '_blank', 'noopener,noreferrer');
+        }
+    };
+
     return (
         <div className='post'>
-            <div className='post-content'>
+            <div className='post-content' onClick={handleClick}>
               <img src={post.cover} alt={post.title} />
               <h1>{post.title}</h1>
               <p>{post.body}</p>
