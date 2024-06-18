82e1ee5d2f930bf6e90e2ac403583fbf8aa96c31
WendrilXX <wendriltutu@gmail.com>
2024-06-18 11:38:16 -0300

    feat: Atualizar componente Card com dados dinâmicos

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -9,24 +9,24 @@ function Card({ tituloAbordagem, informacoesAbordagem }) {
                <p className='titulo-abordagem'>{tituloAbordagem}</p>
                <p className='informacoes-abordagem'>{informacoesAbordagem}</p>
            </div>
-           <div className='retangulo'>
+           {/* <div className='retangulo'>
                <img src="../src/assets/seta.png" alt="seta" className='seta' />
-           </div>
+           </div> */}
         </div>
     );
 }
 
 // Exemplos de uso do componente Card
 const cards = [
-    { tituloAbordagem: "Tecnologia assistiva ", informacoesAbordagem: "Informações a mais sobre Abordagem 1" },
-    { tituloAbordagem: "Abordagem 2", informacoesAbordagem: "Informações a mais sobre Abordagem 2" },
+    { tituloAbordagem: "Tecnologia assistiva", informacoesAbordagem: "Informações a mais sobre Abordagem 1" },
+    { tituloAbordagem: "Terapia Assistida por Animais (TAA)", informacoesAbordagem: "Informações a mais sobre Abordagem 2" },
     { tituloAbordagem: "Abordagem 3", informacoesAbordagem: "Informações a mais sobre Abordagem 3" },
     { tituloAbordagem: "Abordagem 4", informacoesAbordagem: "Informações a mais sobre Abordagem 4" }
 ];
 
 function App() {
     return (
-        <div>
+        <div className='corpo'>
             {cards.map((card, index) => (
                 <Card key={index} {...card} />
             ))}
